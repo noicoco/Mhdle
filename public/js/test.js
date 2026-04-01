effd3f7684a379fab4891437b87f801a5fabc04a
AV <54771755+EndOfLine-py@users.noreply.github.com>
2026-04-01 14:11:53 -0400

    Reworked Icons, Cleaned Structure, Added Icons
    Modified existing icons, cleaned HTML structure, removed unecessary elements, and added a better footer, aiming for less absolute position.
    Buttons are in progress

--- public/js/test.js
+++ public/js/test.js
@@ -1,10 +1,10 @@
-{
-    const max = 4; // Numbers from 0 to 4 possible
-    const randomInt = Math.floor(Math.random() * 5) + 1;
-
-    let background = document.getElementById("background");
-    background.style.backgroundImage = `url("../assets/images/bg${randomInt}.jpg")`;
-}
+// {
+//     const max = 4; // Numbers from 0 to 4 possible
+//     const randomInt = Math.floor(Math.random() * 5) + 1;
+//
+//     let background = document.getElementById("background");
+//     background.style.backgroundImage = `url("../assets/images/bg/bg${randomInt}.jpg")`;
+// }
 
 function toggleParamPanel() {
     let paramPanelId = document.getElementById("paramPanel");
@@ -14,4 +14,16 @@ function toggleParamPanel() {
     } else {
         paramPanelId.style.display = "none";
     }
+}
+
+{
+    const monsterPage = document.getElementById("monsterTablePage");
+    const weaponPage = document.getElementById("monsterTablePage");
+    const compendiumPage = document.getElementById("monsterTablePage");
+    const roarPage = document.getElementById("monsterTablePage");
+    const talentPage = document.getElementById("monsterTablePage");
+
+    const pageButtons = [monsterPage, weaponPage, compendiumPage, roarPage, talentPage]
+
+
 }
