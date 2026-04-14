25643986cfbf57c06ffda864b15ead9d184308db
End <54771755+EndOfLine-py@users.noreply.github.com>
2026-04-14 00:14:56 -0400

    Create routeButton.vue
    Enough for tonight.

---
+++ src/components/routeButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {RouterLink} from "vue-router";
+
+defineProps({ name: String })
+</script>
+
+<template>
+<div class="button">
+  <RouterLink to="/{{ name }}">{{ name }}</RouterLink>
+</div>
+</template>
+
+<style scoped>
+.button {
+  
+}
+</style>
